d7f78442d5e1f4747435f1a86cfb560aef15491f
Byeong Kim <byeong.kim0430@gmail.com>
2019-03-15 15:13:44 -0700

    Add dot at the end of build

--- deploy-master.sh
+++ deploy-master.sh
@@ -1,5 +1,5 @@
 $(aws ecr get-login --no-include-email --region ${AWS_REGION})
-docker build --build-arg CI_COMMIT_SHA=$TRAVIS_COMMIT -t $REPOSITORY_URL:$TRAVIS_COMMIT -t $REPOSITORY_URL:latest
+docker build --build-arg CI_COMMIT_SHA=$TRAVIS_COMMIT -t $REPOSITORY_URL:$TRAVIS_COMMIT -t $REPOSITORY_URL:latest .
 docker push $REPOSITORY_URL
 # Wait for 20 minutes for deploy
 ecs deploy --region ${AWS_REGION} ${PRODUCTION_CLUSTER_NAME} ${PRODUCTION_SERVICE_NAME} --timeout 3000 --tag $TRAVIS_COMMIT --ignore-warnings
